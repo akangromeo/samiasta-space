921f865220d119d216b5c2283a2641bd25c1653f
akangromeo <deanagnia7@gmail.com>
2025-09-19 16:08:49 +0800

    fix : Phone text

--- app/components/ContactSection.vue
+++ app/components/ContactSection.vue
@@ -13,7 +13,7 @@
       Perum. Gapura Citra Dewata Jl. Kebo Iwa Utara No. D/6, Padangsambian Kaja,
       Kec. Denpasar Bar., Kota Denpasar, Bali 80117
     </p>
-    <p class="text-xl pt-2 font-semibold text-dark-green">Whatsapp</p>
+    <p class="text-xl pt-2 font-semibold text-dark-green">Phone</p>
     <p class="text-gray-500">(0361) 3610042</p>
   </div>
 </template>
